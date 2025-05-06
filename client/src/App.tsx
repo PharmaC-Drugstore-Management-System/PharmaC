0b183cc223b5a7514c1e8277b59666449c109c36
llSaiMonDll <zerotwotv@gmail.com>
2025-05-06 13:41:57 +0700

    [Frontend] medication_dashboard
    Compoents

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import RevenueTable from "../components/revenue_table.tsx"
+import MedicationDashboard from "../components/medication_dashbaord.tsx"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <div >
           
           <RevenueTable/>
+          <MedicationDashboard/>
       </div>
      
     </>
