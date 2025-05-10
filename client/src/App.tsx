eb1e4f54a6656f8d9ea6ed9dd30a90044b6b233e
DeOxy <81672195+Ratchanon-Promsombut@users.noreply.github.com>
2025-05-10 22:42:42 +0700

    [Update] App.tsx
    Update router path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
-// import Navbar from './component/Navbar'
-import PharmacInventory from './pages/PharmacInventoryPage'
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PharmacInventoryPage from './pages/PharmacInventoryPage';
+import AddMedicinePage from './pages/AddMedicinePage';
 
+function App() {
   return (
     <>
-     {/* <Navbar/> */}
-     <PharmacInventory/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PharmacInventoryPage />} />
+          <Route path="/add-medicine" element={<AddMedicinePage />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
