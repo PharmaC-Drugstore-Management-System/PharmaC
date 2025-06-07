920da8aa47b055fd7faae989a8057608ad31aa70
llSaiMonDll <zerotwotv@gmail.com>
2025-06-07 20:33:28 +0700

    [UPDATED] bun.lock, index.ts, package

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { pool } from './db';
+
+const server = Bun.serve({
+  port: 3000,
+  async fetch(req) {
+    const result = await pool.query('SELECT * FROM medicines');
+    return new Response(JSON.stringify(result.rows), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
