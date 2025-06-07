f01fe8d1ab3c725ce9474c01f397add12d8452af
llSaiMonDll <zerotwotv@gmail.com>
2025-06-07 23:50:15 +0700

    [UPDATE] header, App, index.css
    index - adjust for print
    App - add new routes
    header - change bg color

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../components/Navbar.tsx";
 import Header from "../components/Header.tsx";
 import TrendPage from "../pages/Trend_page.tsx";
-
 import RevenueDetail from "../pages/RevenueDetail.tsx";
+import POEdit from "../pages/POEdit.tsx";
+import POForm from "../pages/POForm.tsx";
+import PODoc from "../pages/PODoc.tsx";
+
 function App() {
   return (
-
-<Router>
+    <Router>
       <div className="flex h-screen">
         {/* Sidebar */}
         <Navbar />
@@ -24,13 +26,15 @@ function App() {
             <Routes>
               <Route path="/trend" element={<TrendPage />} />
               {/* <Route path="/" element={<Dashboard />} /> */}
-              <Route path="/RevenueDetail" element={<RevenueDetail />} />
+              <Route path="/revenuedetail" element={<RevenueDetail />} />
+              <Route path="/poedit" element={<POEdit />} />
+              <Route path="/poform" element={<POForm />} />
+              <Route path="/podoc" element={<PODoc />} />
             </Routes>
           </div>
         </div>
       </div>
     </Router>
-
   );
 }
 
