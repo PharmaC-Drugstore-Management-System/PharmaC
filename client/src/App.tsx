e3f07019d6098acd3a7d7c2953927d09d1fa936e
DeOxy <81672195+Ratchanon-Promsombut@users.noreply.github.com>
2025-06-07 23:41:12 +0700

    [Update]

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,57 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from './components/Navbar.tsx';
 import Header from './components/Header';
 // import PharmacInventoryPage from "./pages/PharmacInventoryPage.tsx";
 import AddMedicinePage from "./pages/AddMedicinePage.tsx";
 import MedicineDetailPage from "./pages/MedicineDetailPage.tsx";
+import PharmacInventoryPage from "./pages/PharmacInventoryPage.tsx";
+import SettingsPage from "./pages/settingPage.tsx";
+import AccountPage from "./pages/accountPage.tsx";
+import ThemePage from "./pages/ThemePage.tsx";
 // import Dashboard from './pages/Dashboard.tsx';
+
+function AppContent() {
+  const location = useLocation();
+  const isSettingsPage = location.pathname === '/settings';
+  const isAccountPage = location.pathname === '/accountSetting';
+  const isThemePage = location.pathname === '/pageSetting';
+  const hideNavAndHeader = isSettingsPage || isAccountPage || isThemePage;
+
+  return (
+    <div className="flex h-screen">
+      {/* Conditionally render Navbar */}
+      {!hideNavAndHeader && <Navbar />}
+      
+      {/* Main content */}
+      <div className="flex flex-col flex-1 overflow-hidden">
+        {/* Conditionally render Header */}
+        {!hideNavAndHeader && (
+          <div className="sticky top-0 z-10">
+            <Header />
+          </div>
+        )}
+        
+        {/* Page content */}
+        <div className={`flex-1 overflow-y-auto ${!hideNavAndHeader ? 'p-6' : ''}`}>
+          <Routes>
+            <Route path="/" element={<MedicineDetailPage />} />
+            <Route path="/add-medicine" element={<AddMedicinePage />} />
+            <Route path="/inventory" element={<PharmacInventoryPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/accountSetting" element={<AccountPage />} />
+            <Route path="/pageSetting" element={< ThemePage/>} />
+            {/* <Route path="/medicine-details" element={<MedicineDetailPage />} /> */}
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div className="flex h-screen">
-        {/* {/* Sidebar /} */}
-        <Navbar />
-
-        {/* {/ Main content /} */}
-        <div className="flex flex-col flex-1 overflow-hidden">
-          {/* {/ Fixed Header /} */}
-          <div className="sticky top-0 z-10">
-            <Header />
-          </div>
-
-          {/* {/ Page content */} 
-          <div className="flex-1 overflow-y-auto p-4">
-            <Routes>
-              <Route path="/" element={<MedicineDetailPage />} />
-              <Route path="/add-medicine" element={<AddMedicinePage />} />
-              {/* <Route path="/medicine-details" element={<MedicineDetailPage />} /> */}
-
-            </Routes>
-          </div>
-        </div>
-      </div>
+      <AppContent />
     </Router>
   );
 }
