4b2b86283e186b58b49b3427d4e4b81d861a7ae1
llSaiMonDll <zerotwotv@gmail.com>
2025-08-18 16:39:18 +0700

    [DELETED] change database

--- server/src/routes/iscontrolled.routes.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from "express";
-import controller from '../controller/iscontrolled.controller.ts';
-
-const router = Router();
-router.post('/add-controlled', controller.add);
-router.get('/get-controlled', controller.get);
-
-export default router;
--- server/src/routes/type.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import controller from '../controller/type.controller.ts'
-const router = Router();
-
-router.post('/add-type', controller.add);
-router.get('/get-type', controller.get)
-
-
-export default router;
