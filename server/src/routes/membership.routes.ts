84b7aca1dec991c3f2b402a99741345f4fde08f3
llSaiMonDll <zerotwotv@gmail.com>
2025-08-24 23:38:54 +0700

    [DELETED]

--- server/src/routes/membership.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import controller from '../controller/membership.controller.ts'
-
-const router = Router();
-
-router.post('/add-member', controller.add);
-router.get('/get-member', controller.get);
-
-export default router;
